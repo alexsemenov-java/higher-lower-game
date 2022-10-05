2e9bd751d1bf6ea8320aebdb45d55a397c517df6
Alexander Semenov <asemenov.java@gmail.com>
2022-10-05 14:16:43 +0300

    Added a cycle for the ability to start a new one by entering a value from the user

--- src/HiLo.java
+++ src/HiLo.java
@@ -3,34 +3,48 @@ import java.util.Scanner;
 public class HiLo {
     public static void main(String[] args) {
 
-        int hiddenNumber = (int)(Math.random() * 100 +1); // (0.0...0.99999999) * 100 + 1
 
-        int userNumber = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        while (userNumber != hiddenNumber) {
+        String playAgain = "";
 
-            System.out.println("Введите число от 1 до 100:");
+        do {
 
-            userNumber = scanner.nextInt();
+            int hiddenNumber = (int)(Math.random() * 100 +1); // (0.0...0.99999999) * 100 + 1
 
-            if (userNumber < hiddenNumber){
+            int userNumber = 0;
 
-                System.out.println( userNumber +  " меньше загаданного. Ввведите еще число");
+            while (userNumber != hiddenNumber) {
 
-            } else if (userNumber > hiddenNumber) {
+                System.out.println("Введите число от 1 до 100:");
 
-                System.out.println(userNumber + " больше загаданного. Введите еще число");
+                userNumber = scanner.nextInt();
 
-            }else {
+                if (userNumber < hiddenNumber){
 
-                System.out.println(userNumber + " это загаданное число. Вы победили!");
+                    System.out.println( userNumber +  " меньше загаданного. Ввведите еще число");
+
+                } else if (userNumber > hiddenNumber) {
+
+                    System.out.println(userNumber + " больше загаданного. Введите еще число");
+
+                }else {
+
+                    System.out.println(userNumber + " это загаданное число. Вы победили!");
+                }
 
             }
 
+            System.out.println("Хотите сыграть снова (y/n)?");
 
-        }
+            playAgain = scanner.next();
+
+        } while (playAgain.equalsIgnoreCase("y"));
+
+        System.out.println("Спасибо за игру! Пока!");
+
+        scanner.close();
 
     }
 }
